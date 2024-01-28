f69bb918c31d43f6f246c3609ea134dffa112469
ZIQING YU <ziqing.yu0623@gmail.com>
2024-01-28 23:17:32 +0100

    finish view of trend and view of table

--- src/gui_main.py
+++ src/gui_main.py
@@ -4,20 +4,24 @@ Created on Sun Jan 28 13:47:32 2024
 
 @author: yuziq
 """
-
+import copy
 import PySimpleGUI as sg
 import lib_extract_data as led
+import pandas as pd
+from matplotlib import pyplot as plt 
 sg.theme("DarkBlue3")
 sg.set_options(font=("Microsoft JhengHei", 16))
 class BilligenceCaseStudy():
     def __init__(self):
         self.dataframe = None
         self.fields = []
+        self.df_dict_in_year = {}
+        self.df_dict_in_institution = {}
         self.main()
 
     def main(self):
         layout_load = [  [sg.Text('Functions')],
-                    [sg.Button('Load data'), sg.Button('Compare data in same year'),\
+                    [sg.Button('Load data'), sg.Button('Compare data in one year'),\
                      sg.Button('Compare trend'), sg.Button('Prediction')],\
                     [sg.Button('Cancel')]]
         
@@ -29,6 +33,10 @@ class BilligenceCaseStudy():
             else:
                 if event == 'Load data':
                     self.gui_read_data()
+                elif event == 'Compare data in one year':
+                    self.gui_table_select_view()
+                elif event == 'Compare trend':
+                    self.gui_trend_view()
                 # load new window according to the event
         window.close()
 
@@ -46,6 +54,7 @@ class BilligenceCaseStudy():
             elif event == 'Ok':
                 file = values[0]
 
+
             dataframe_temp = led.read_data_from_file(file)
             if self.dataframe is None:
                 self.dataframe = dataframe_temp
@@ -53,19 +62,124 @@ class BilligenceCaseStudy():
                 pass
                 # self.dataframe = led.combine_df(self.dataframe,dataframe.temp)      
             self.fields,self.df_dict_in_year,self.df_dict_in_institution = \
-                led.update_df(self.dataframe)    
-            print(self.dataframe)
+                led.update_df(self.dataframe) 
+            break
         window.close()
     
-    def gui_table_view(self):
-        layout_table = [  [sg.Text('Load the data')],
-                    [sg.Text('Give the data here'),sg.Input(),sg.FileBrowse()],
-                    [sg.Button('Ok'), sg.Button('Cancel')] ]
-        
-        window = sg.Window('UK Uni Case Study', layout_table)
+    def gui_table_select_view(self):
+        lst_year = sg.Combo(list(self.df_dict_in_year.keys()), \
+                            expand_x=True, enable_events=True, key='Ranking Year')
+        layout_table = [[sg.Text('Ranking Year:'),lst_year],
+                        [sg.Text('  ')]]
+        table_type_dict = led.row_type_for_table(self.dataframe, self.fields)
 
+        for item in self.fields:
+            if not (item == 'Ranking Year'):
+                if table_type_dict[item]['type'] == 'range':
+                    vmin = table_type_dict[item]['min']
+                    vmax = table_type_dict[item]['max']
+                    layout_table.append([sg.Text(item),\
+                                         sg.Text(f'Min (from {vmin})'),\
+                                             sg.Input(key=f'min_{item}',size=(5, 2)),\
+                                         sg.Text(f'Max (to {vmax})'),\
+                                             sg.Input(key=f'max_{item}',size=(5, 2)),])
+                '''elif table_type_dict[item]['type'] == 'multiselect':
+                    names = table_type_dict[item]['values']
+                    layout_table.append([sg.Text(item),sg.Listbox(names, \
+                                expand_y=True, enable_events=True, key=f'list_{item}')])'''
+        layout_table.append([sg.Button('OK'),sg.Button('Cancel')])
+        window = sg.Window('UK Uni Case Study', layout_table)
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+                break
+            elif event == 'OK':
+                # generate the table
+                self.gui_table_view(table_type_dict, values)
+        window.close()
+    
+    def gui_table_view(self,table_type_dict,values):
+        condition = led.filter_choose(self.dataframe,'Ranking Year',[values['Ranking Year']])
+        for item in table_type_dict:
+            if item == 'Ranking Year':
+                continue
+            elif table_type_dict[item]['type'] == 'range':
+                if values[f'min_{item}'] == '':
+                    rmin = table_type_dict[item]['min']
+                else:
+                    rmin = float(values[f'min_{item}'])
+                if values[f'max_{item}'] == '':
+                    rmax = table_type_dict[item]['max']
+                else:
+                    rmax = float(values[f'max_{item}'])
+                condition_temp = led.filter_range(self.dataframe, item, rmin, rmax)
+                condition = condition & condition_temp
+
+        df_present = self.dataframe.loc[condition,:]
+        df_present = df_present.round(2)
+        values = df_present.values.tolist()
+        sg.theme("DarkBlue3")
+        sg.set_options(font=("Courier New", 12))
+        headings = self.fields
+        for item in headings:
+            item.replace(' ','\n')
+        layout = [[sg.Table(values = values, headings = headings,
+            # Set column widths for empty record of table
+            auto_size_columns=True,
+            col_widths=list(map(lambda x:len(x)+1, self.fields)))]]
+
+        window = sg.Window('Sample excel file',  layout)
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED:
+                break
+        window.close()
+    
     def gui_trend_view(self):
-        pass
+        fields_plot = copy.deepcopy(self.fields)
+        fields_plot.remove('Ranking Year')
+        fields_plot.remove('Institution')
+        layout_trend = [[sg.Text('Select the Institutions'),\
+                         sg.Listbox(list(self.df_dict_in_institution.keys()), \
+                         size=(20, 5),expand_y=True, enable_events=True,\
+                            select_mode=sg.LISTBOX_SELECT_MODE_EXTENDED, key='institution')],\
+                        [sg.Text('Select the factors'),sg.Listbox(fields_plot, \
+                         size=(20, 5),expand_y=True, enable_events=True,\
+                            select_mode=sg.LISTBOX_SELECT_MODE_EXTENDED, key='factors')],
+                        [sg.Button('OK'),sg.Button('Cancel')]]
+        window = sg.Window('Trend',  layout_trend)
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED or event == 'Cancel':
+                break
+            elif event == 'OK':
+                time_plot = []
+                fields_chosen = values['factors']
+                for i,item1 in enumerate(fields_chosen):
+                    fig,ax = plt.subplots()
+                    for j,item2 in enumerate(values['institution']):     
+                        data = self.df_dict_in_institution[item2][item1]
+                        time = self.df_dict_in_institution[item2]['Ranking Year']
+                        # add previous ranking into ranking
+                        if item1 == 'Ranking':
+                            data2 = self.df_dict_in_institution[item2]['Ranking (Prev)'].iloc[0]
+                            data = pd.Series([data2]+list(data))
+                        else:
+                            data = pd.Series([None]+list(data))
+                        time2 = time.iloc[0]-1
+                        time = pd.Series([time2]+list(time))
+                        # document time
+                        if len(time_plot)<len(time):
+                            time_plot = time
+                        # plot
+                        ax.plot(time,data)
+                    ax.set_xticks(time_plot)
+                    ax.set_xticklabels(time_plot)
+                    ax.legend(values['institution'])
+                    ax.set_ylabel(item1)
+                    ax.set_xlabel('Year')
+                    fig.show()
+        window.close() 
     
     def gui_prediction_view(self):
         pass
